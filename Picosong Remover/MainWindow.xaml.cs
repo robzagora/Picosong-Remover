81eba340081c4799a1b66197ca4d3054ea32f5f3
Robert Zagorskis <zagorskis.roberts@yahoo.com>
2017-06-03 14:40:28 +0100

    Add basic UI and invoker code.

--- Picosong Remover/MainWindow.xaml.cs
+++ Picosong Remover/MainWindow.xaml.cs
@@ -1,20 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace Picosong_Remover
+﻿namespace Picosong.Remover
 {
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Windows;
+    using System.Windows.Controls;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +15,39 @@ namespace Picosong_Remover
         public MainWindow()
         {
             InitializeComponent();
+
+            this.Execute.IsEnabled = false;
+        }
+
+        private void Execute_Click(object sender, RoutedEventArgs e)
+        {
+            string value = this.Links.Text;
+
+            value = value.Replace(Environment.NewLine, ",");
+
+            string[] values = value.Split(',').Where(v => !string.IsNullOrWhiteSpace(v)).Select(v => @"http://picosong.com/delete/" + v.Split('/').Last()).ToArray();
+
+            foreach (string uri in values)
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadStringAsync(new Uri(uri));
+                }
+            }
+
+            this.State.Visibility = Visibility.Visible;
+        }
+
+        private void Links_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (this.Links.Text.Length == 0)
+            {
+                this.Execute.IsEnabled = false;
+            }
+            else
+            {
+                this.Execute.IsEnabled = true;
+            }
         }
     }
 }
